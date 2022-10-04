4fc55d8662cfedf593a96de68e14a49abfe8b57d
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2022-10-04 20:06:24 -0300

    Atualizando setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="calculator",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    author="Marcus",
+    author_email="my_email@myemail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/marcusmarinhob/dio-criacao-pacotes"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
